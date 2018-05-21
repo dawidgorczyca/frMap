f42fe73cc16bb070b9d3a279a57b395aa5c8b72f
Grzegorz Kasperski <grzegorz.kasperski@synapse.com>
2018-05-21 15:06:11 +0200

    Filled in some of definitions and node instances for map

--- src/data/definitions.js
+++ src/data/definitions.js
@@ -26,8 +26,8 @@ export const SidebarComponent = {
 }
 
 export const TopbarComponent = {
-  nodeName: 'Sidebar Component',
-  component: 'Sidebar',
+  nodeName: 'Topbar Component',
+  component: 'Topbar',
   description: '',
   propTypes: [],
   dispatchers: [],
@@ -53,7 +53,7 @@ export const UserMenuComponent = {
 }
 
 export const NavigationMenuComponent = {
-  nodeName: 'Navigation Component',
+  nodeName: 'Navigation Menu Component',
   component: 'NavigationMenu',
   description: '',
   propTypes: [],
@@ -124,8 +124,17 @@ export const GoJSComponent = {
   subscriptions: [],
 }
 
-export const SlidingPanelComponent = {
-  nodeName: 'Sliding Panel Component',
+export const SlidingPanelRightSideComponent = {
+  nodeName: 'Sliding Panel Right Side Component',
+  component: 'SlidingPanel',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SlidingPanelLeftSideComponent = {
+  nodeName: 'Sliding Panel Left Side Component',
   component: 'SlidingPanel',
   description: '',
   propTypes: [],
@@ -142,7 +151,7 @@ export const DeviceCatalogComponent = {
   subscriptions: [],
 }
 
-export const DeviceCatalogSearchComponent = {
+export const DeviceCatalogSearch = {
   nodeName: 'Device Catalog Search Component',
   component: 'DeviceCatalogSearch',
   description: '',
@@ -151,7 +160,7 @@ export const DeviceCatalogSearchComponent = {
   subscriptions: [],
 }
 
-export const DeviceImportComponent = {
+export const DeviceImport = {
   nodeName: 'DeviceImport Component',
   component: 'DeviceImport',
   description: '',
@@ -160,7 +169,7 @@ export const DeviceImportComponent = {
   subscriptions: [],
 }
 
-export const DeviceCatalogFilterComponent = {
+export const DeviceCatalogFilter = {
   nodeName: 'Device Catalog Filter Component',
   component: 'DeviceCatalogFilter',
   description: '',
@@ -169,9 +178,540 @@ export const DeviceCatalogFilterComponent = {
   subscriptions: [],
 }
 
-export const DeviceCatalogListComponent = {
+export const DeviceCatalogList = {
   nodeName: 'Device Catalog List Component',
   component: 'Device Catalog List',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const ProjectConfigurator = {
+  nodeName: 'ProjectConfigurator Component',
+  component: 'Project Configurator',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const OutsideAlerter = {
+  nodeName: 'OutsideAlerter Component',
+  component: 'Outside Alerter',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const WebviewElement = {
+  nodeName: 'WebviewElement Component',
+  component: 'WebviewElement',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const ModalLog = {
+  nodeName: 'ModalLog Component',
+  component: 'ModalLog',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const Button = {
+  nodeName: 'Button Component',
+  component: 'Button',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const ScanManager = {
+  nodeName: 'ScanManager Component',
+  component: 'ScanManager',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DevicesScanManager = {
+  nodeName: 'DevicesScanManager Component',
+  component: 'DevicesScanManager',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const InterfacesScanManager = {
+  nodeName: 'InterfacesScanManager Component',
+  component: 'InterfacesScanManager',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const InterfacesList = {
+  nodeName: 'InterfacesList Component',
+  component: 'InterfacesList',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const NoResultsView = {
+  nodeName: 'NoResultsView Component',
+  component: 'NoResultsView',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SingleInterfaceList = {
+  nodeName: 'SingleInterfaceList Component',
+  component: 'SingleInterfaceList',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const CheckboxButton = {
+  nodeName: 'CheckboxButton Component',
+  component: 'CheckboxButton',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const NetworkRangeInput = {
+  nodeName: 'NetworkRangeInput Component',
+  component: 'NetworkRangeInput',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SimpleListHeader = {
+  nodeName: 'SimpleListHeader Component',
+  component: 'SimpleListHeader',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SimpleDottedProgress = {
+  nodeName: 'SimpleDottedProgress Component',
+  component: 'SimpleDottedProgress',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DevicesList = {
+  nodeName: 'DevicesList Component',
+  component: 'DevicesList',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const ListItemBackground = {
+  nodeName: 'ListItemBackground Component',
+  component: 'ListItemBackground',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const Spinner = {
+  nodeName: 'Spinner Component',
+  component: 'Spinner',
+  description: 'An npm component',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const TabularViewDevices = {
+  nodeName: 'TabularViewDevices Component',
+  component: 'TabularViewDevices',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const TabularViewConnections = {
+  nodeName: 'TabularViewConnections Component',
+  component: 'Spinner',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const FlatButton = {
+  nodeName: 'FlatButton Component',
+  component: 'FlatButton',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const HiddenComponentAuthorization = {
+  nodeName: 'HiddenComponentAuthorization Component',
+  component: 'HiddenComponentAuthorization',
+  description: 'Gives feature to hide component or replace it with information message or another component passed in props.  Component is hidden based on a flag in access configuration file',
+  propTypes: ['id', 'roles'],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const WithAuthorization = {
+  nodeName: 'WithAuthorization Component',
+  component: 'WithAuthorization',
+  description: 'Creates an element used to replace an original component based on access config file.',
+  propTypes: ['id', 'msg', 'ReplacementWrapper'],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const Dropzone = {
+  nodeName: 'Dropzone Component',
+  component: 'Dropzone',
+  description: 'Npm component https://github.com/okonet/react-dropzone',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DeviceImportListItem = {
+  nodeName: 'DeviceImportListItem Component',
+  component: 'DeviceImportListItem',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DropzonePlaceholder = {
+  nodeName: 'DropzonePlaceholder Component',
+  component: 'DropzonePlaceholder',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DropzoneLoading = {
+  nodeName: 'DropzoneLoading Component',
+  component: 'DropzoneLoading',
+  description: '',
+  propTypes: ['progress'],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const CircularProgressbar = {
+  nodeName: 'CircularProgressbar Component',
+  component: 'CircularProgressbar',
+  description: 'Npm component',
+  propTypes: ['percentage'],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const FilterDevicesForm = {
+  nodeName: 'FilterDevicesForm Component',
+  component: 'FilterDevicesForm',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const FilterDevicesList = {
+  nodeName: 'FilterDevicesList Component',
+  component: 'FilterDevicesList',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DeviceCatalogItem = {
+  nodeName: 'DeviceCatalogItem Component',
+  component: 'DeviceCatalogItem',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const BasicInputComponent = {
+  nodeName: 'BasicInputComponent Component',
+  component: 'BasicInputComponent',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DeviceSummary = {
+  nodeName: 'DeviceSummary Component',
+  component: 'DeviceSummary',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const ConnectionInfo = {
+  nodeName: 'ConnectionInfo Component',
+  component: 'ConnectionInfo',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const GenericTcpIpForm = {
+  nodeName: 'GenericTcpIpForm Component',
+  component: 'GenericTcpIpForm',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SlidingPanelTopBar = {
+  nodeName: 'SlidingPanelTopBar Component',
+  component: 'SlidingPanelTopBar',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SlidingPanelTabs = {
+  nodeName: 'SlidingPanelTabs Component',
+  component: 'SlidingPanelTabs',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DeviceStatus = {
+  nodeName: 'DeviceStatus Component',
+  component: 'DeviceStatus',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const MasterConfigurationForm = {
+  nodeName: 'MasterConfigurationForm Component',
+  component: 'MasterConfigurationForm',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const ValidationComponent = {
+  nodeName: 'ValidationComponent Component',
+  component: 'ValidationComponent',
+  description: '@balluf npm package',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const TextArea = {
+  nodeName: 'TextArea Component',
+  component: 'TextArea',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const TextInput = {
+  nodeName: 'TextInput Component',
+  component: 'TextInput',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const CommonFormSection = {
+  nodeName: 'CommonFormSection Component',
+  component: 'CommonFormSection',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const Form = {
+  nodeName: 'Form Component',
+  component: 'Form',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const FormFieldsGroup = {
+  nodeName: 'FormFieldsGroup Component',
+  component: 'FormFieldsGroup',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DisableAuthorization = {
+  nodeName: 'DisableAuthorization Component',
+  component: 'DisableAuthorization',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const RaisedButton = {
+  nodeName: 'RaisedButton Component',
+  component: 'RaisedButton',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SlidingPanelButton = {
+  nodeName: 'SlidingPanelButton Component',
+  component: 'SlidingPanelButton',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const Tooltip = {
+  nodeName: 'Tooltip Component',
+  component: 'Tooltip',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const OverlayTrigger = {
+  nodeName: 'OverlayTrigger Component',
+  component: 'OverlayTrigger',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const SlidingPanelTabsContent = {
+  nodeName: 'SlidingPanelTabsContent Component',
+  component: 'SlidingPanelTabsContent',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DeviceCommonInfo = {
+  nodeName: 'DeviceCommonInfo Component',
+  component: 'DeviceCommonInfo',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DeviceGenericInfo = {
+  nodeName: 'DeviceGenericInfo Component',
+  component: 'DeviceGenericInfo',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const Parameters = {
+  nodeName: 'Parameters Component',
+  component: 'Parameters',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const TabsPanelBody = {
+  nodeName: 'TabsPanelBody Component',
+  component: 'TabsPanelBody',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const TabsPanelRow = {
+  nodeName: 'TabsPanelRow Component',
+  component: 'TabsPanelRow',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DevicePropertyAccessRights = {
+  nodeName: 'DevicePropertyAccessRights Component',
+  component: 'DevicePropertyAccessRights',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const DevicePropertyState = {
+  nodeName: 'DevicePropertyState Component',
+  component: 'DevicePropertyState',
+  description: '',
+  propTypes: [],
+  dispatchers: [],
+  subscriptions: [],
+}
+
+export const TabsPanelCell = {
+  nodeName: 'TabsPanelCell Component',
+  component: 'TabsPanelCell',
   description: '',
   propTypes: [],
   dispatchers: [],
--- src/data/instances.js
+++ src/data/instances.js
@@ -1,91 +1,427 @@
 import * as definitions from './definitions';
 
-// Topbar
+export const SpinnerInstance = {
+  ...definitions.Spinner
+}
+
+export const MenuInstance = {
+  ...definitions.MenuComponent
+}
+
+export const UserMenuInstance = {
+  ...definitions.UserMenuComponent
+}
+
+export const WebviewElementInstance = {
+  ...definitions.WebviewElement
+}
+
+export const ButtonInstance = {
+  ...definitions.Button
+}
+
 export const SettingsInstance = {
   ...definitions.SettingsComponent
+}
+
+export const SimpleDottedProgressInstance = {
+  ...definitions.SimpleDottedProgress
+}
+
+export const ListItemBackgroundInstance = {
+  ...definitions.ListItemBackground
+}
+
+export const SimpleListHeaderInstance = {
+  ...definitions.SimpleListHeader
+}
+
+export const NoResultsViewInstance = {
+  ...definitions.NoResultsView
+}
+
+export const CheckboxButtonInstance = {
+  ...definitions.CheckboxButton
+}
+
+export const NetworkRangeInputInstance = {
+  ...definitions.NetworkRangeInput
+}
+
+export const GoJSComponentInstance = {
+  ...definitions.GoJSComponent
+}
+
+export const WithAuthorization = {
+  ...definitions.WithAuthorization
+}
+
+export const HiddenComponentAuthorization = {
+  ...definitions.HiddenComponentAuthorization,
+  children: [
+    WithAuthorization
+  ]
+}
+
+export const FlatButtonInstance = {
+  ...definitions.FlatButton,
+  children: [
+    HiddenComponentAuthorization
+  ]
+}
+
+export const ZoomElementInstance = {
+  ...definitions.ZoomElementComponent,
+  children: [
+    FlatButtonInstance
+  ]
+}
+
+export const TopologyEditorInstance = {
+  ...definitions.TopologyEditorComponent,
+  children: [
+    ZoomElementInstance,
+    GoJSComponentInstance
+  ]
+}
+
+export const DevicesListInstance = {
+  ...definitions.DevicesList,
+  children: [
+    SimpleDottedProgressInstance,
+    CheckboxButtonInstance,
+    SimpleListHeaderInstance,
+    ListItemBackgroundInstance,
+    NoResultsViewInstance,
+  ]
+}
+
+
+
+export const DropdownInstance = {
+  ...definitions.DropdownComponent,
+  children: [
+    { ...definitions.OutsideAlerter },
+  ],
+}
+
+export const DevicesScanManagerInstance = {
+  ...definitions.DevicesScanManager,
+  children: [
+    SimpleDottedProgressInstance,
+    SpinnerInstance,
+    DevicesListInstance,
+    ButtonInstance,
+    SimpleListHeaderInstance
+  ]
+}
+
+export const SingleInterfaceListInstance = {
+  ...definitions.SingleInterfaceList,
+  children: [
+    CheckboxButtonInstance,
+    ButtonInstance,
+    NetworkRangeInputInstance,
+    SimpleListHeaderInstance
+  ]
+}
+
+export const InterfacesListInstance = {
+  ...definitions.InterfacesList,
+  children: [
+    NoResultsViewInstance,
+    SingleInterfaceListInstance
+  ]
+}
+
+export const InterfacesScanManagerInstance = {
+  ...definitions.InterfacesScanManager,
+  children: [
+    InterfacesListInstance,
+    ButtonInstance
+  ]
+}
+
+export const ScanManagerInstance = {
+  ...definitions.ScanManager,
+  children: [
+    DevicesScanManagerInstance,
+    InterfacesScanManagerInstance
+  ]
 }
 
 export const TopbarInstance = {
   ...definitions.TopbarComponent,
   children: [
-    {
-      ...definitions.MenuComponent,
-    },
-    {
-      ...definitions.MenuComponent,
-      children: [
-        {
-          ...definitions.DropdownComponent,
-          children: [
-            SettingsInstance
-          ]
-        }
-      ]
-    },
+    MenuInstance,
+    DropdownInstance,
+    ScanManagerInstance
   ]
 }
 
-// Sidebar
-export const UserMenuInstance = {
-  ...definitions.UserMenuComponent
+export const ModalLogInstance = {
+  ...definitions.ModalLog,
+  children: [
+    ButtonInstance
+  ]
 }
 
 export const NavigationMenuInstance = {
-  ...definitions.NavigationMenuComponent
+  ...definitions.NavigationMenuComponent,
+  children: [
+    ModalLogInstance,
+    WebviewElementInstance
+  ]
 }
 
 export const SidebarInstance = {
   ...definitions.SidebarComponent,
   children: [
     UserMenuInstance,
-    {
-      ...definitions.MenuComponent,
-      children: [
-        { ...definitions.DropdownComponent }
-      ]
-    },
+    MenuInstance,
+    DropdownInstance,
     NavigationMenuInstance
   ]
 }
 
 // Sections
-export const LeftSideInstance = {
-  ...definitions.SlidingPanelComponent,
+export const TabularDevicesInstance = {
+  ...definitions.TabularViewDevices
+}
+
+export const TabularConnectionsInstance = {
+  ...definitions.TabularViewConnections
+}
+
+export const MasterConfigurationFormInstance = {
+  ...definitions.MasterConfigurationForm,
   children: [
-    {
-      ...definitions.DeviceCatalogComponent,
-      children: [
-        {
-          ...definitions.DeviceCatalogSearchComponent
-        },
-        {
-          ...definitions.DeviceImportComponent
-        },
-        {
-          ...definitions.DeviceCatalogFilterComponent
-        },
-        {
-          ...definitions.DeviceCatalogListComponent
-        }
-      ]
-    }
+    { ...definitions.BasicInputComponent }
+  ]
+}
+
+export const DeviceSummaryInstance = {
+  ...definitions.DeviceSummary,
+  children: [
+    { ...definitions.DeviceStatus },
+    MasterConfigurationFormInstance
+  ]
+}
+
+export const ConnectionInfoInstance = {
+  ...definitions.ConnectionInfo,
+  children: [
+    { ...definitions.BasicInputComponent },
+    { ...definitions.ValidationComponent },
+  ]
+}
+
+export const TextAreaInstance = {
+  ...definitions.TextArea,
+  children: [
+    { ...definitions.DisableAuthorization }
+  ]
+}
+
+
+export const GenericTcpIpFormInstance = {
+  ...definitions.GenericTcpIpForm,
+  children: [
+    TextAreaInstance,
+    { ...definitions.Form },
+    { ...definitions.FormFieldsGroup },
+    { ...definitions.CommonFormSection },
+  ]
+}
+
+export const RaisedButtonInstance = {
+  ...definitions.RaisedButton,
+  children: [
+    { ...definitions.Tooltip },
+    { ...definitions.OverlayTrigger }
+  ]
+}
+
+export const SlidingPanelTopBarInstance = {
+  ...definitions.SlidingPanelTopBar,
+  children: [
+    RaisedButtonInstance,
+    { ...definitions.SlidingPanelButton },
+  ]
+}
+
+export const TextInputInstance = {
+  ...definitions.TextInput,
+  children: [
+    { ...definitions.Tooltip },
+    { ...definitions.OverlayTrigger },
+    { ...definitions.DisableAuthorization },
+  ]
+}
+
+
+export const DeviceCommonInfoInstance = {
+  ...definitions.DeviceCommonInfo,
+  children: [
+    TextAreaInstance,
+    TextInputInstance,
+  ]
+}
+
+export const ParametersInstance = {
+  ...definitions.Parameters,
+  children: [
+    ButtonInstance,
+    { ...definitions.BasicInputComponent }
+  ]
+}
+
+export const DeviceGenericInfoInstance = {
+  ...definitions.DeviceGenericInfo,
+  children: [
+    CheckboxButtonInstance,
+    ButtonInstance,
+    ParametersInstance
+  ]
+}
+
+export const TabsPanelCellInstance = {
+  ...definitions.TabsPanelCell,
+  children: [
+    { ...definitions.BasicInputComponent },
+    { ...definitions.ValidationComponent },
+  ]
+}
+
+export const DevicePropertyStateInstance = {
+  ...definitions.DevicePropertyAccessRights,
+  children: [
+    TabsPanelCellInstance
+  ]
+}
+
+export const TabsPanelRowInstance = {
+  ...definitions.TabsPanelRow,
+  children: [
+    TabsPanelCellInstance,
+    DevicePropertyStateInstance,
+    { ...definitions.DevicePropertyAccessRights }
+  ]
+}
+
+export const TabsPanelBodyInstance = {
+  ...definitions.TabsPanelBody,
+  children: [
+    TabsPanelRowInstance
+  ]
+}
+
+
+export const SlidingPanelTabsContentInstance = {
+  ...definitions.SlidingPanelTabsContent,
+  children: [
+    TabsPanelBodyInstance
+  ]
+}
+
+export const SlidingPanelTabsInstance = {
+  ...definitions.SlidingPanelTabs,
+  children: [
+    DeviceCommonInfoInstance,
+    DeviceGenericInfoInstance,
+    { ...definitions.SlidingPanelButton },
+    SlidingPanelTabsContentInstance
+  ]
+}
+
+
+export const RightSideInstance = {
+  ...definitions.SlidingPanelRightSideComponent,
+  children: [
+    DeviceSummaryInstance,
+    ConnectionInfoInstance,
+    GenericTcpIpFormInstance,
+    SlidingPanelTopBarInstance,
+    SlidingPanelTabsInstance,
+  ]
+}
+
+export const DropzoneLoadingInstance = {
+  ...definitions.DropzoneLoading,
+  children: [
+    { ...definitions.CircularProgressbar }
+  ]
+}
+
+export const DeviceImportInstance = {
+  ...definitions.DeviceImport,
+  children: [
+    { ...definitions.Dropzone },
+    { ...definitions.DeviceImportListItem },
+    { ...definitions.DropzonePlaceholder },
+    DropzoneLoadingInstance,
+  ]
+}
+
+export const FilterDevicesList = {
+  ...definitions.FilterDevicesList,
+  children: [
+    ButtonInstance,
+    CheckboxButtonInstance
+  ]
+}
+
+export const FilterDevicesForm = {
+  ...definitions.FilterDevicesForm,
+  children: [
+    ButtonInstance,
+    FilterDevicesList
+  ]
+}
+
+export const DeviceCatalogFilterInstance = {
+  ...definitions.DeviceCatalogFilter,
+  children: [
+    FilterDevicesForm
+  ]
+}
+
+export const DeviceCatalogListInstance = {
+  ...definitions.DeviceCatalogList,
+  children: [
+    { ...definitions.DeviceCatalogItem },
+    NoResultsViewInstance
+  ]
+}
+
+export const DeviceCatalogSearchInstance = {
+  ...definitions.DeviceCatalogSearch,
+  children: [
+    { ...definitions.BasicInputComponent }
+  ]
+}
+
+export const DeviceCatalogComponentInstance = {
+  ...definitions.DeviceCatalogComponent,
+  children: [
+    DeviceImportInstance,
+    DeviceCatalogFilterInstance,
+    DeviceCatalogListInstance,
+    DeviceCatalogSearchInstance
+  ]
+}
+
+export const LeftSideInstance = {
+  ...definitions.SlidingPanelLeftSideComponent,
+  children: [
+    DeviceCatalogComponentInstance
   ]
 }
 
 export const TopologyViewInstance = {
   ...definitions.TopologyViewComponent,
   children: [
-    {
-      ...definitions.TopologyEditorComponent,
-      children: [
-        {
-          ...definitions.ZoomElementComponent
-        },
-        {
-          ...definitions.GoJSComponent
-        }
-      ]
-    }
+    TopologyEditorInstance
   ]
 }
 
@@ -94,7 +430,10 @@ export const ContentBaseInstance = {
   ...definitions.ContentBaseComponent,
   children: [
     TopologyViewInstance,
-    LeftSideInstance
+    LeftSideInstance,
+    RightSideInstance,
+    TabularDevicesInstance,
+    TabularConnectionsInstance
   ]
 }
 
